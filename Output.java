57de40bea9a18c4bc4ff1f626f388f5a89f57998
aryan24-cs <aryan.nda.2163@gmail.com>
2025-07-17 22:06:14 +0530

    Create Output.java

---
+++ Output.java
@@ -0,0 +1,36 @@
+public class Output {
+    public static void main(String[] args) {
+
+        // Syntax
+        // System.out.println("");
+
+        //this line is used to get output in java
+        System.out.println("hello World");
+
+        // for printing next line 
+        System.out.println("hello Aryan");
+        System.out.print("hey users");
+
+        //another way to print next line:
+        System.out.print("hey\n");
+        System.out.print("hello\n");
+
+        // Example
+        System.out.println("hello World With Java\nFrom \nAryan");
+
+        // question:
+        // print the pattern:
+        // *
+        // **
+        // ***
+        // ****
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
+        // this is not the better way to do these things
+
+    }
+}
