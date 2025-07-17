e5ddb589d2526c16ad089f4389cbb44054e0ba10
aryan24-cs <aryan.nda.2163@gmail.com>
2025-07-17 22:20:30 +0530

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,31 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        // Variables: these are those quantity that change its value.
+        // constant: those value do not change.
+
+        // Variable Syntax in JAVA:  
+        // Datatype variableName = value;
+
+        String name = "Aryan";
+        int a = 25 ; 
+        int b = 12 ; 
+        double price = 25.25;
+
+        // DataTypes:
+        // there are 2 types of data types: [1. Primitive DataTypes] [2.Non-Primitive DataTypes]
+
+        //Primitive: byte, short, char, boolean, int, long, float, double
+        //Non-Primitive: Arrays, Strings, Class, Objects, Interfaces.
+
+        int x =10;
+        int y= 25; 
+        int sum = x+y;
+        // System.out.println(sum);
+        int diff = x-y;
+        // System.out.println(diff);
+        int mul = x*y;
+        // System.out.println(mul);
+
+    }
+}
