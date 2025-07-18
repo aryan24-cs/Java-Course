34221c22792f862e8fef51006df54891e2801481
aryan24-cs <aryan.nda.2163@gmail.com>
2025-07-18 22:55:13 +0530

    updated

---
+++ Basics/Conditional_Statements.java
@@ -0,0 +1,6 @@
+package Basics;
+public class Conditional_Statements {
+    public static void main(String[] args) {
+        
+    }
+}
--- Basics/Output.java
+++ Basics/Output.java
@@ -1,3 +1,4 @@
+package Basics;
 public class Output {
     public static void main(String[] args) {
 
