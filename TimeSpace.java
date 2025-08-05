3594da974db38f1f8a44331b2b63d3110b262a3d
aryan24-cs <aryan.nda.2163@gmail.com>
2025-08-05 22:20:09 +0530

    time and space complexity

---
+++ TimeSpace.java
@@ -0,0 +1,30 @@
+public class TimeSpace {
+    public static void main(String[] args) {
+        //time and space complexity
+
+        //Time Complexity: relation between input size and time take by the algorithm
+
+        //it can be constant, linear, logarithmic, quadratic, etc.
+
+        // there are different tyoe of cases in time complexity
+        
+        // 1. Best Case: the minimum time taken by the algorithm for any input size.omega notation is used to represent the best case.
+        
+
+        // 2. Average Case: the average time taken by the algorithm for all possible inputs of a given size.omega notation is used to represent the average case.
+
+        // 3. Worst Case: the maximum time taken by the algorithm for any input size.big O notation is used to represent the worst case.
+
+
+        //Space Complexity: relation between input size and space taken by the algorithm
+
+        //it can be constant, linear, logarithmic, quadratic, etc.
+
+        //it is usually represented using big O notation as well, similar to time complexity.
+
+        //Example: 
+        //For an algorithm that processes an array of size n:
+        //Time Complexity: O(n) - linear time complexity, as it processes each element once.
+        //Space Complexity: O(1) - constant space complexity, if it uses a fixed amount of extra space regardless of input size.
+    }
+}
