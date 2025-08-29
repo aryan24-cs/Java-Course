a358aab3fdc0772aa292d38b5b6c7e3dae4b2376
aryan24-cs <aryan.nda.2163@gmail.com>
2025-08-29 21:49:21 +0530

    updates

--- Arrays.java
+++
@@ -1,9 +0,0 @@
-public class Arrays {
-    public static void main(String[] args) {
-        // Arrays in java
-
-        // these are used to store multiple values in a single variable, instead of declaring separate variables for each value.
-        // An array is a collection of items stored at contiguous memory locations.
-        // It is a data structure that can store a fixed-size sequential collection of elements of the same type.
-    }
-}
